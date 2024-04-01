1a3faf5639c3c7ad280ced436ebac8aa6588488d
apurvakhatri <apurvakhatri2011@gmail.com>
2024-04-01 18:07:11 -0400

    courses

--- App.js
+++ App.js
@@ -1,22 +1,14 @@
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
+import CourseRoutes from "./Kanbas/courses/routes.js";
 import express from 'express';
 import cors from "cors";
 
 
 const app = express();
 app.use(cors());
+CourseRoutes(app);
 app.use(express.json());
 Lab5(app);
 Hello(app);
 app.listen(4000)
-
-
-// app.use(cors({
-//     credentials: true,
-//     origin: process.env.FRONTEND_URL
-// }));
-// CourseRoutes(app);
-// ModuleRoutes(app);
-// AssignmentRoutes(app);
-// app.listen(process.env.PORT || 4000);
