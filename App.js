0526faf3524f252bb4e09fc616ce486fe1f01ee7
apurvakhatri <apurvakhatri2011@gmail.com>
2024-04-01 19:20:39 -0400

    Modules added

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
 import express from 'express';
 import cors from "cors";
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(cors());
 CourseRoutes(app);
 app.use(express.json());
+ModuleRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(4000)
