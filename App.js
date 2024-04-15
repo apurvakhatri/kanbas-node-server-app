45b2e8c289e3fd0215cd0beda8849991bb104f53
apurvakhatri <apurvakhatri2011@gmail.com>
2024-04-15 18:22:20 -0400

    3.5.6 Added

--- App.js
+++ App.js
@@ -1,18 +1,40 @@
-import Hello from "./Hello.js";
-import Lab5 from "./Lab5.js";
+import express from 'express';
+import mongoose from "mongoose";
+import cors from "cors";
+
+// Import routes
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from "./Kanbas/assignments/routes.js";
-import express from 'express';
-import cors from "cors";
-
+import UserRoutes from "./Users/routes.js";
+import Lab5 from "./Lab5.js";
+import Hello from "./Hello.js";
 
 const app = express();
-app.use(cors());
-CourseRoutes(app);
-app.use(express.json());
-ModuleRoutes(app);
-AssignmentRoutes(app);
-Lab5(app);
-Hello(app);
-app.listen(process.env.PORT || 4000);
+
+// Establish MongoDB connection
+mongoose.connect("mongodb+srv://khatriapurva2011:PAoHhNCReuZMq7lF@cluster0.zcqxcfq.mongodb.net/kanbas")
+  .then(() => {
+    console.log("MongoDB connected successfully!");
+    console.log(mongoose.connection.readyState);
+    // Set up middleware
+    app.use(cors());
+    app.use(express.json());
+
+    // Set up routes
+    CourseRoutes(app);
+    UserRoutes(app);
+    ModuleRoutes(app);
+    AssignmentRoutes(app);
+    Lab5(app);
+    Hello(app);
+
+    // Start the server
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
